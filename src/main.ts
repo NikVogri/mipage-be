895e7f8bc59916002163b90d241df32b97c1ac50
NikVogri <55229210+NikVogri@users.noreply.github.com>
2022-02-26 01:22:19 +0100

    fix: cookie domain & remove redundant console log

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  console.log(process.env.URL_ORIGIN);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
