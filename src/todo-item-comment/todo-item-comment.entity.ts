18588c0a20b168bfa0b726ebc7ab5f2d32f0f8b1
NikVogri <55229210+NikVogri@users.noreply.github.com>
2022-02-28 18:14:08 +0100

    feat: todo item comment serializer, dto and entity

---
+++ src/todo-item-comment/todo-item-comment.entity.ts
@@ -0,0 +1,31 @@
+import { TodoItem } from 'src/todo-item/todo-item.entity';
+import { User } from 'src/user/user.entity';
+import {
+  Entity,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  Column,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+
+@Entity()
+export class TodoItemComment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  body: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => TodoItem, { onDelete: 'CASCADE' })
+  todoItem: TodoItem;
+
+  @ManyToOne(() => User)
+  author: User;
+}
