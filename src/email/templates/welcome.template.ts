d23b63384c8b8bfd787ec2a353f78123fb2148dc
NikVogri <vogrinec.nik@gmail.com>
2022-04-17 20:14:09 +0200

    feat: email templates

---
+++ src/email/templates/welcome.template.ts
@@ -0,0 +1,15 @@
+export default `
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="x-apple-disable-message-reformatting">
+        <title></title>
+    </head>
+    <body>
+        <h3>Hi, ##USERNAME##!</h3>
+        <p>You have successfully registered to Mipage. We're glad to have you with us!!</p>
+    </body>
+</html>
+`;
