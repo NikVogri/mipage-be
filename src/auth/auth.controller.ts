7229a75174060a36c8bf3f9a4e4352a440086dbe
NikVogri <vogrinec.nik@gmail.com>
2022-04-23 16:52:42 +0200

    fix: auth cookie now gets correctly cleared

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -43,12 +43,7 @@ export class AuthController {
 
   @Post('/logout')
   async logout(@Res() response: Response) {
-    response
-      .cookie('mipage-auth', null, {
-        expires: new Date(Date.now() - 3600), // set expire to date in the past
-      })
-      .status(200)
-      .send({ success: true });
+    response.clearCookie('mipage-auth').status(200).send({ success: true });
   }
 
   @Post('/forgot-password')
