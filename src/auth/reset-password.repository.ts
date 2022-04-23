87d6a34239007883e8537f29d209f67165316226
NikVogri <vogrinec.nik@gmail.com>
2022-04-23 16:18:57 +0200

    feat: reset password repository and entity

---
+++ src/auth/reset-password.repository.ts
@@ -0,0 +1,28 @@
+import * as crypto from 'crypto';
+
+import { EntityRepository, Repository } from 'typeorm';
+import { ResetPasswordToken } from './reset-password-token.entity';
+
+@EntityRepository(ResetPasswordToken)
+export class ResetPasswordRepository extends Repository<ResetPasswordToken> {
+  generateRandomToken(): string {
+    return crypto.randomBytes(48).toString('hex');
+  }
+
+  async savePasswordResetToken(userId: string): Promise<string> {
+    const token = this.generateRandomToken();
+
+    const resetTokenRecord = this.create({
+      token: token,
+      userId,
+    });
+
+    await this.save(resetTokenRecord);
+
+    return token;
+  }
+
+  async getToken(token: string): Promise<ResetPasswordToken> {
+    return await this.findOne({ token });
+  }
+}
