400426c38b4d4d082b5e89af7ef5f5643cd3222f
NikVogri <vogrinec.nik@gmail.com>
2022-11-09 21:46:10 +0100

    feat: sync authentication validation with FE

--- src/auth/dto/reset-password.dto.ts
+++ src/auth/dto/reset-password.dto.ts
@@ -2,10 +2,7 @@ import { IsString, Length, Matches, MinLength } from 'class-validator';
 
 export class ResetPasswordDto {
   @MinLength(8)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message:
-      'Password must include at least 1 uppercase letter and at least 1 number',
-  })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
   password: string;
 
   @IsString()
